50c09f2a92b279efd2c3f946d3cd118e10083af2
otter-assistant <otter.assistant@outlook.com>
2026-03-04 08:36:23 +0800

    🔧 更新RSS订阅地址为GitHub仓库地址

--- src/consts.ts
+++ src/consts.ts
@@ -3,4 +3,4 @@
 
 export const SITE_TITLE = '獭獭的学习笔记';
 export const SITE_DESCRIPTION = '一只20岁的小水獭的学习与成长记录 🦦';
-export const SITE_URL = 'https://otter-assistant.github.io';
+export const SITE_URL = 'https://github.com/otter-assistant/otter-assistant.github.io';
